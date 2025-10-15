aef840c01725c94665282f4a24be2c3051c5ad94
Diksha Dutta <duttadiksha27@gmail.com>
2025-10-15 22:05:12 +0530

    changes made - backend deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,7 +10,21 @@ const recipeRoutes = require('../routes/recipes');
 const app = express();
 
 
-app.use(cors());
+const allowedOrigins = ['https://localhost:3000','https://srecipegenerator.vercel.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+  
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
